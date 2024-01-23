16cb2653763770395d9e341b9c4e88158928ccc0
Alex Vaisero <vaiserowork@gmail.com>
2024-01-23 18:37:23 +0300

    Display of days of the week AND stable operation of switching months and years

--- TestTask_CIROBS/wwwroot/js/site.js
+++ TestTask_CIROBS/wwwroot/js/site.js
@@ -1,4 +1,58 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+$(document).ready(function () {
+    var months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
 
-// Write your JavaScript code.
+    var currentDate = new Date();
+    var currentMonth = currentDate.getMonth();
+    var currentYear = currentDate.getFullYear();
+    var selectedDate = currentDate;
+
+    // Отображение текущего месяца и года
+    $("#monthYear").text(months[currentMonth] + " " + currentYear);
+
+    // Создание и заполнение таблицы с датами
+    fillCalendar(currentMonth, currentYear);
+
+    // Переключение месяцев по стрелкам
+    $("#prevBtn").click(function () {
+        if (currentMonth === 0) {
+            currentMonth = 11;
+            currentYear -= 1;
+        } else {
+            currentMonth -= 1;
+        }
+        $("#monthYear").text(months[currentMonth] + " " + currentYear);
+        fillCalendar(currentMonth, currentYear);
+    });
+
+    $("#nextBtn").click(function () {
+        if (currentMonth === 11) {
+            currentMonth = 0;
+            currentYear += 1;
+        } else {
+            currentMonth += 1;
+        }
+        $("#monthYear").text(months[currentMonth] + " " + currentYear);
+        fillCalendar(currentMonth, currentYear);
+    });
+
+    // Заполнение таблицы с датами
+    function fillCalendar(month, year) {
+        var firstDay = new Date(year, month, 1).getDay();
+        var daysInMonth = new Date(year, month + 1, 0).getDate();
+
+        var calendarBody = $("#calendarBody");
+        calendarBody.empty();
+
+        var date = 1;
+        for (var i = 0; i < 6; i++) {
+            var row = $("<tr></tr>");
+            for (var j = 0; j < 7; j++) {
+                if (i === 0 && j < firstDay) {
+                    var cell = $("<td></td>");
+                }
+            }
+        }
+    }
+
+})
