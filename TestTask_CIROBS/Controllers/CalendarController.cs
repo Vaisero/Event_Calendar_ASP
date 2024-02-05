9392fe901dafcacc84fcc627cb85011c18b9b3df
Alex Vaisero <vaiserowork@gmail.com>
2024-02-05 07:59:47 +0300

    Calendar Controller function GET from database

---
+++ TestTask_CIROBS/Controllers/CalendarController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+using System.Diagnostics;
+using TestTask_CIROBS.Models;
+
+namespace TestTask_CIROBS.Controllers
+{
+    public class CalendarController : Controller
+    {
+        
+        public IActionResult GetEvent(DateTime date) 
+        {
+            using (var connection = new NpgsqlConnection("ConnectionString"))
+            {
+                connection.Open();
+
+                using (var command = new NpgsqlCommand("event_read", connection))
+                {
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("p_date", date);
+
+                    using (var reader = command.ExecuteReader()) 
+                    {
+                        if (reader.Read())
+                        {
+                            var EventInfo = new
+                            {
+                                event_name = reader.GetString(0),
+                                event_date = reader.GetString(1),
+                                category_id = reader.GetString(2)
+                            };
+
+                            return Json(EventInfo);
+                        }
+                        else
+                        {
+                            return NotFound();
+                        }
+                    }
+                }
+
+            }
+        }
+        
+
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- TestTask_CIROBS/Program.cs
+++ TestTask_CIROBS/Program.cs
@@ -14,7 +14,7 @@ namespace TestTask_CIROBS
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Calendar/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -24,11 +24,9 @@ namespace TestTask_CIROBS
 
             app.UseRouting();
 
-            app.UseAuthorization();
-
             app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+               name: "default",
+               pattern: "{controller=Calendar}/{action=Index}/{id?}");
 
             app.Run();
         }
